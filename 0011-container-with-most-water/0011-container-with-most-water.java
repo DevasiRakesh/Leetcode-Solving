5d2a1ff1ddac943d8984f258d3065254ed9929da
DevasiRakesh <devasirakhi123@gmail.com>
2025-07-15 22:45:06 +0530

    Time: 5 ms (75.28%), Space: 57.9 MB (55%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,18 +1,26 @@
+// int l=0,maxWaterStore=0;
+//         for(int r=1;r<height.length;r++){
+//             int minHeight=Math.min(height[l],height[r]);
+//             maxWaterStore=Math.max(minHeight*(r-l),maxWaterStore);
+//             if(height[l]<height[r]){
+//                 int idx=r+1;
+//                 while(idx<height.length){
+//                     int minHeight1=Math.min(height[l],height[idx]);
+//                     maxWaterStore=Math.max(minHeight1*(idx-l),maxWaterStore);
+//                     idx++;
+//                 }
+//                 l=r;
+//             }
+//         }
+//         return maxWaterStore;
 class Solution {
     public int maxArea(int[] height) {
-        int l=0,maxWaterStore=0;
-        for(int r=1;r<height.length;r++){
-            int minHeight=Math.min(height[l],height[r]);
-            maxWaterStore=Math.max(minHeight*(r-l),maxWaterStore);
-            if(height[l]<height[r]){
-                int idx=r+1;
-                while(idx<height.length){
-                    int minHeight1=Math.min(height[l],height[idx]);
-                    maxWaterStore=Math.max(minHeight1*(idx-l),maxWaterStore);
-                    idx++;
-                }
-                l=r;
-            }
+        int l=0,r=height.length-1,maxWaterStore=0;
+        while(l<r){
+            int minH=Math.min(height[l],height[r]);
+            maxWaterStore=Math.max(maxWaterStore,minH*(r-l));
+            if(height[l]<height[r]){l++;}
+            else{r--;}
         }
         return maxWaterStore;
     }
