02ea0b045daad509f25120d79e4b794bd0244930
DevasiRakesh <devasirakhi123@gmail.com>
2025-07-17 19:08:28 +0530

    Time: 12 ms (24.8%), Space: 46.5 MB (20.71%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.java
+++ 0382-linked-list-random-node/0382-linked-list-random-node.java
@@ -11,7 +11,9 @@
 class Solution {
     List<Integer> list=new ArrayList<>();
     Random rand = new Random();
+    ListNode head;
     public Solution(ListNode head) {
+        this.head=head;
         ListNode temp=head;
         while(temp!=null){
             list.add(temp.val);
@@ -19,9 +21,23 @@ class Solution {
         }
     }
     public int getRandom() {
-        int n=list.size();
-        int random_Index=rand.nextInt(n);
-        return list.get(random_Index);
+        // int n=list.size();
+        // int random_Index=rand.nextInt(n);
+        // return list.get(random_Index);
+        int result = head.val;
+        ListNode curr = head.next;
+        int index = 1;
+
+        while (curr != null) {
+            if (rand.nextInt(index + 1) == 1) {
+                result = curr.val;
+            }
+            index++;
+            curr = curr.next;
+        }
+
+        // return result;
+        return result;
     }
 }
 
