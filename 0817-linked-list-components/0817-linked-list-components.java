4531a6b4d906856a242ad3ecbdab5c61aba049c4
DevasiRakesh <devasirakhi123@gmail.com>
2025-07-18 12:12:28 +0530

    Time: 8 ms (41.94%), Space: 46 MB (18.09%) - LeetHub

--- 0817-linked-list-components/0817-linked-list-components.java
+++ 0817-linked-list-components/0817-linked-list-components.java
@@ -10,23 +10,35 @@
  */
 class Solution {
     public int numComponents(ListNode head, int[] nums) {
-         ListNode temp=head;
-         HashSet<Integer> set=new HashSet<>();
-         int connect=0,nc=0;
-         for(int num:nums){
-            set.add(num);
-         }
-         while(temp!=null){
-            if(set.contains(temp.val)){
-                boolean check=false;
-                while(temp!=null && set.contains(temp.val)){
-                    temp=temp.next;
-                    check=true;
-                }
-                if(check){connect++;}else{nc++;}
-            }else{temp=temp.next;}
-         }
-         return nc+connect;
+        //  ListNode temp=head;
+        //  HashSet<Integer> set=new HashSet<>();
+        //  int connect=0,nc=0;
+        //  for(int num:nums){
+        //     set.add(num);
+        //  }
+        //  while(temp!=null){
+        //     if(set.contains(temp.val)){
+        //         boolean check=false;
+        //         while(temp!=null && set.contains(temp.val)){
+        //             temp=temp.next;
+        //             check=true;
+        //         }
+        //         if(check){connect++;}else{nc++;}
+        //     }else{temp=temp.next;}
+        //  }
+        //  return nc+connect;
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+
+        int count = 0;
+        while (head != null) {
+            if (set.contains(head.val) &&
+                (head.next == null || !set.contains(head.next.val))) {
+                count++;
+            }
+            head = head.next;
+        }
+        return count;
 
     }
 }
