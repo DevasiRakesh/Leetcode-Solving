cb8d465d6c34cf4a8d9b1ebf8253f5fd3662a441
DevasiRakesh <devasirakhi123@gmail.com>
2025-07-21 19:05:38 +0530

    Time: 1 ms (93.44%), Space: 45.5 MB (66.34%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -10,26 +10,34 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        if(head==null){return null;}
-        ListNode curr=head,prev=null;
-        while(curr!=null){
-            boolean check=false;
-            if(curr.val==val){
-                while(curr!=null && curr.val==val){
-                    curr=curr.next;
-                }
-                check=true;
-            }
-            if(check){
-                if(prev==null){prev=head=curr;}
-                else{
-                    prev.next=curr;check=false;
-                }
-                continue;
-            }
-            prev=curr;
-            curr=curr.next;
+        // if(head==null){return null;}
+        // ListNode curr=head,prev=null;
+        // while(curr!=null){
+        //     boolean check=false;
+        //     if(curr.val==val){
+        //         while(curr!=null && curr.val==val){
+        //             curr=curr.next;
+        //         }
+        //         check=true;
+        //     }
+        //     if(check){
+        //         if(prev==null){prev=head=curr;}
+        //         else{
+        //             prev.next=curr;check=false;
+        //         }
+        //         continue;//because curr is null here in some case
+        //     }
+        //     prev=curr;
+        //     curr=curr.next;
+        // }
+        // return head;
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        ListNode curr=dummy;
+        while(curr.next!=null){
+            if(curr.next.val==val){curr.next=curr.next.next;}
+            else{curr=curr.next;}
         }
-        return head;
+        return dummy.next;
     }
 }
