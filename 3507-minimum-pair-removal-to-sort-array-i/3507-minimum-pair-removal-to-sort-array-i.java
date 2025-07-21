8c2e2553305f9ddb02dc208c484f64f1ca198bf6
DevasiRakesh <devasirakhi123@gmail.com>
2025-07-21 20:12:30 +0530

    Time: 2 ms (63.32%), Space: 42.9 MB (38.29%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.java
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.java
@@ -1,11 +1,25 @@
 class Solution {
-    public int minimumPairRemoval(int[] nums) {
-        int len=nums.length,cnt=0;
-        for(int i=len-1;i>=1;i--){
-            if(nums[i]<nums[i-1]){
-                nums[i-1]=nums[i]+nums[i-1];
-                System.out.println(nums[i-1]);cnt++;}
+    public static boolean hasInversion(List<Integer> list){
+        for(int i=0;i<list.size()-1;i++){
+            if(list.get(i)>list.get(i+1)) return true;
         }
-        return cnt;
+        return false;
+    }
+    public int minimumPairRemoval(int[] nums) {
+        List<Integer> list=new ArrayList<>();
+        for(int i:nums){list.add(i);}
+        int idx=-1,sum=0,ans=0;
+        while(hasInversion(list)){
+            int len=list.size();
+            int minSum=Integer.MAX_VALUE;
+            for(int i=0;i<len-1;i++){
+                sum=list.get(i)+list.get(i+1);
+                if(sum<minSum){minSum=sum;idx=i;}
+            }
+            list.set(idx,minSum);
+            list.remove(idx+1);
+            ans++;
+        }
+        return ans;
     }
 }
