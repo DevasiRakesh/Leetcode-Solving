f918f6bdb54a15757d4f81208aecaa84c1a6b616
DevasiRakesh <devasirakhi123@gmail.com>
2025-07-24 10:20:08 +0530

    Time: 0 ms (100%), Space: 41.4 MB (90.86%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,38 +1,61 @@
-public class Solution {
-    int idx = 0;
-
+class Solution {
     public String decodeString(String s) {
-        return dfs(s);
+        return dfs(s,new int[]{0});
     }
-
-    private String dfs(String s) {
-        StringBuilder result = new StringBuilder();
-
-        while (idx < s.length()) {
-            char c = s.charAt(idx);
-
-            if (Character.isDigit(c)) {
-                int num = 0;
-                while (Character.isDigit(s.charAt(idx))) {
-                    num = num * 10 + (s.charAt(idx) - '0');
-                    idx++;
+    public static String dfs(String s,int[] idxRef){
+        StringBuilder result=new StringBuilder();
+        while(idxRef[0]<s.length()){
+            char ch=s.charAt(idxRef[0]);
+            if(Character.isDigit(ch)){
+                int num=0;
+                while(Character.isDigit(s.charAt(idxRef[0]))){
+                    num=num*10 + s.charAt(idxRef[0])-'0';
+                    idxRef[0]++;
                 }
-
-                idx++; // skip '['
-                String decoded = dfs(s);
-                idx++; // skip ']'
-
-                for (int i = 0; i < num; i++) {
-                    result.append(decoded);
-                }
-            } else if (c == ']') {
-                break;
-            } else {
-                result.append(c);
-                idx++;
-            }
+                idxRef[0]++;//skip '['
+                String decode=dfs(s,idxRef);
+                idxRef[0]++;//skip ']'
+                for(int i=0;i<num;i++){result.append(decode);}
+            }else if(ch==']'){break;}
+            else{result.append(ch);idxRef[0]++;}
         }
-
         return result.toString();
     }
 }
+// public class Solution {
+//     int idx = 0;
+
+//     public String decodeString(String s) {
+//         return dfs(s);
+//     }
+
+//     private String dfs(String s) {
+//         StringBuilder result = new StringBuilder();
+
+//         while (idx < s.length()) {
+//             char c = s.charAt(idx);
+
+//             if (Character.isDigit(c)) {
+//                 int num = 0;
+//                 while (Character.isDigit(s.charAt(idx))) {
+//                     num = num * 10 + (s.charAt(idx) - '0');
+//                     idx++;
+//                 }
+
+//                 idx++; // skip '['
+//                 String decoded = dfs(s);
+//                 idx++; // skip ']'
+
+//                 for (int i = 0; i < num; i++) {
+//                     result.append(decoded);
+//                 }
+//             } else if (c == ']') {
+//                 break;
+//             } else {
+//                 result.append(c);
+//                 idx++;
+//             }
+//         }
+
+//         return result.toString();
+//     }
