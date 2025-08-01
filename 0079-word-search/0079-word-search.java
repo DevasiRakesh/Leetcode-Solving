7781cf44bc1f029ddc554cf483b00cf3f8332757
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-01 11:55:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -12,11 +12,11 @@ class Solution {
     public static boolean backtrack(char[][] board,String word,int i,int j,int idx){
         if(idx==word.length()){return true;}
         if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]!=word.charAt(idx)){return false;}
-        char temp=board[i][j];
+        // char temp=board[i][j];
         board[i][j]='#';
         boolean found = backtrack(board,word,i+1,j,idx+1)|| backtrack(board,word,i-1,j,idx+1) ||
                         backtrack(board,word,i,j+1,idx+1)|| backtrack(board,word,i,j-1,idx+1);
-        board[i][j]=temp;
+        // board[i][j]=temp;
         return found;
     }
 }
