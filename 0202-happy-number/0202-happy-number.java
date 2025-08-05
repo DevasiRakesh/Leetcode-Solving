75b396743894cefb5d4b4365329448f35f30ea35
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-05 11:00:15 +0530

    Time: 2 ms (18.07%), Space: 41.3 MB (8.81%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,18 +1,37 @@
 class Solution {
     public boolean isHappy(int n) {
-        int sum=0;
-        HashSet<Integer> set=new HashSet<>();
-        set.add(n);
-        while(n>0){
-            int ud=n%10;
-            sum+=ud*ud;
-            n/=10;
-            if(n==0){
-                if(sum==1){return true;}
-                else if(!set.contains(sum)){set.add(sum);n=sum;sum=0;}
-                else{return false;}
-            }
+    //     int sum=0;
+    //     HashSet<Integer> set=new HashSet<>();
+    //     set.add(n);
+    //     while(n>0){
+    //         int ud=n%10;
+    //         sum+=ud*ud;
+    //         n/=10;
+    //         if(n==0){
+    //             if(sum==1){return true;}
+    //             else if(!set.contains(sum)){set.add(sum);n=sum;sum=0;}
+    //             else{return false;}
+    //         }
+    //     }
+    //     return true;
+    // }
+    Set<Integer> seen = new HashSet<>();
+        
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getSquareSum(n);
         }
-        return true;
+
+        return n == 1;
+    }
+
+    private int getSquareSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n = n / 10;
+        }
+        return sum;
     }
 }
