f5e4190cb549933e02dbf18cdcbef582f5c124ee
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-07 14:19:59 +0530

    Time: 2 ms (99.84%), Space: 50.6 MB (27.48%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,24 +1,5 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        // List<Integer> lst=new ArrayList<>();
-        // int cnt1=0,cnt2=0,cd1=0,cd2=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(cnt1==0){
-        //         cnt1=1;cd1=nums[i];
-        //     }else if(cnt2==0){
-        //         cnt2=1;cd2=nums[i];
-        //     }else if(cd1==nums[i]){cnt1++;}
-        //     else if(cd2==nums[i]){cnt2++;}
-        //     else{cnt1--;cnt2--;}
-        // }
-        // cnt1=cnt2=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]==cd1){cnt1++;}
-        //     else if(nums[i]==cd2){cnt2++;}
-        // }
-        // if(cnt1>nums.length/3){lst.add(cd1);}
-        // if(cnt2>nums.length/3){lst.add(cd2);}
-        // return lst;
         List<Integer> result = new ArrayList<>();
         
         if (nums == null || nums.length == 0) {
@@ -26,7 +7,7 @@ class Solution {
         }
 
         // 1st Pass: Find potential candidates
-        int candidate1 = 0, candidate2 = 0; // initialize with different numbers
+        int candidate1 = 0, candidate2 = 0; 
         int count1 = 0, count2 = 0;
 
         for (int num : nums) {
