30c05ade30eb2c3e2a44f8ecfce59b95a214a2fa
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-07 13:56:28 +0530

    Time: 17 ms (17.25%), Space: 49.2 MB (96.83%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -4,10 +4,10 @@ class Solution {
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int num:nums){
             map.put(num,map.getOrDefault(num,0)+1);
+            if(map.get(num)==(nums.length/3) +1){lst.add(num);}
+
         }
-        for(Integer key:map.keySet()){
-            if(map.get(key)>nums.length/3){lst.add(key);}
-        }
+        
         return lst;
     }
 }
