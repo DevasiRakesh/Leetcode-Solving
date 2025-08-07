d262ad36057fb25f9d2ed246558e1be2e5d1d774
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-07 14:17:40 +0530

    Time: 2 ms (99.84%), Space: 50.4 MB (41.65%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,13 +1,62 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> lst=new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-            if(map.get(num)==(nums.length/3) +1){lst.add(num);}
-
-        }
+        // List<Integer> lst=new ArrayList<>();
+        // int cnt1=0,cnt2=0,cd1=0,cd2=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(cnt1==0){
+        //         cnt1=1;cd1=nums[i];
+        //     }else if(cnt2==0){
+        //         cnt2=1;cd2=nums[i];
+        //     }else if(cd1==nums[i]){cnt1++;}
+        //     else if(cd2==nums[i]){cnt2++;}
+        //     else{cnt1--;cnt2--;}
+        // }
+        // cnt1=cnt2=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==cd1){cnt1++;}
+        //     else if(nums[i]==cd2){cnt2++;}
+        // }
+        // if(cnt1>nums.length/3){lst.add(cd1);}
+        // if(cnt2>nums.length/3){lst.add(cd2);}
+        // return lst;
+        List<Integer> result = new ArrayList<>();
         
-        return lst;
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        // 1st Pass: Find potential candidates
+        int candidate1 = 0, candidate2 = 1; // initialize with different numbers
+        int count1 = 0, count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // 2nd Pass: Validate the candidates
+        count1 = count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
+        int n = nums.length;
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3) result.add(candidate2);
+
+        return result;
     }
 }
