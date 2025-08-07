5bab10eab0d9d1086241e9d3f859ac8aad7d6ff2
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-07 13:39:01 +0530

    Time: 1 ms (99.86%), Space: 53.1 MB (60.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        // moore voting algrorithm
        int cnt=0,el=-1;
        for(int i=0;i<nums.length;i++){
         if(cnt==0){cnt=1;el=nums[i];}
         else if(el==nums[i]){cnt++;}
         else{cnt--;}
        }
-       cnt=0;
-       for(int i=0;i<nums.length;i++){
-        if(nums[i]==el){cnt++;}
-       }
-       if(cnt>nums.length/2){return el;}
-       return -1;
+    //    cnt=0;
+    //    for(int i=0;i<nums.length;i++){
+    //     if(nums[i]==el){cnt++;}
+    //    }
+    //    if(cnt>nums.length/2){return el;}
+       return el;
     }
 }
