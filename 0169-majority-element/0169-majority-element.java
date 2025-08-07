c5bc9bee97f289421e6771103de20f7dbedb0308
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-07 13:21:37 +0530

    Time: 12 ms (36.86%), Space: 49.7 MB (77.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,7 +4,7 @@ class Solution {
         for(int num:nums){
             map.put(num,map.getOrDefault(num,0)+1);
         }
-        for(Integer key:map.keySet()){
+        for(int key:map.keySet()){
             if(map.get(key)>nums.length/2){return key;}
         }
         return 0;
