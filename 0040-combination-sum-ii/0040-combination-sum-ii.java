f6675412cff655f2837658f27334a3302a9c3442
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-09 17:18:43 +0530

    Time: 2 ms (99.8%), Space: 43.3 MB (52.77%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -2,26 +2,26 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(candidates); // Sort to handle duplicates easily
-        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        boolean used[]=new boolean[candidates.length];
+        backtrack(result, new ArrayList<>(), candidates, target, 0,used);
         return result;
     }
 
-    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] candidates, int remain, int start) {
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] candidates, int remain, int start,boolean used[]) {
         if (remain == 0) {
             result.add(new ArrayList<>(temp));
             return;
         }
 
         for (int i = start; i < candidates.length; i++) {
+             if (candidates[i] > remain) return;// arr is sort
             // Skip duplicates (important)
-            if (i > start && candidates[i] == candidates[i - 1]) continue;
-
-            // No need to continue if the number is too big
-            if (candidates[i] > remain) return;// arr is sort
-
+            if (i > 0 && candidates[i] == candidates[i - 1] && !used[i-1]) continue;  
+            used[i]=true;
             temp.add(candidates[i]);
-            backtrack(result, temp, candidates, remain - candidates[i], i + 1); // i + 1: don't reuse
+            backtrack(result, temp, candidates, remain - candidates[i], i + 1,used); // i + 1: don't reuse
             temp.remove(temp.size() - 1); // Backtrack
+            used[i]=false;
         }
     }
 }
