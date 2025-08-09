199ff6bfbe3aae46eab39b5c8f04901529d603a5
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-09 18:31:55 +0530

    Time: 1 ms (99.95%), Space: 43.5 MB (78.81%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,22 +1,24 @@
 class Solution {
-    public static void findSubset(List<List<Integer>> result,List<Integer> list,int num[],int i, boolean isPicked){
+    public static void findSubset(List<List<Integer>> result,List<Integer> list,int num[],int i, boolean used[]){
         if(i==num.length){
             result.add(new ArrayList<>(list));return;
             }
-            findSubset(result,list,num,i+1,false);
-        if(i>= 1 && num[i-1] == num[i] && !isPicked) {
+        findSubset(result,list,num,i+1,used);
+        if(i>= 1 && num[i-1] == num[i] && !used[i-1]) {
             return;
         }
         list.add(num[i]);
-        findSubset(result,list,num,i+1,true);
+        used[i]=true;
+        findSubset(result,list,num,i+1,used);
         list.remove(list.size()-1);
-        
+        used[i]=false;
 
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
          List<List<Integer>> result=new ArrayList<>();
-        findSubset(result,new ArrayList<>(),nums,0,false);
+         boolean used[]=new boolean[nums.length];
+        findSubset(result,new ArrayList<>(),nums,0,used);
         return result;
     }
 }
