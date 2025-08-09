d37e695b4da704556ae4a88fe2b37a19af6cc644
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-09 09:04:52 +0530

    Time: 1 ms (100%), Space: 41.3 MB (68.5%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.java
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.java
@@ -1,43 +1,37 @@
 class Solution {
     public boolean makesquare(int[] matchsticks) {
-        int sum = 0;
-        for (int stick : matchsticks) {
-            sum += stick;
+     int sum =0;
+        for(int stick : matchsticks){
+            sum+= stick;
         }
-        
-        // Total sum must be divisible by 4
-        if (sum % 4 != 0) return false;
-        
-        int side = sum / 4;
-        
-        // Sort descending to speed up pruning
+        if(sum%4 != 0)return false;
+        int target = sum/4;
         Arrays.sort(matchsticks);
-        for (int i = 0; i < matchsticks.length / 2; i++) {
-            int temp = matchsticks[i];
-            matchsticks[i] = matchsticks[matchsticks.length - 1 - i];
-            matchsticks[matchsticks.length - 1 - i] = temp;
-        }
-        
-        int[] sides = new int[4]; // Represents 4 sides of the square
-        
-        return backtrack(matchsticks, 0, sides, side);
+        int length = matchsticks.length;
+        if(matchsticks[length-1]>target) return false;
+        boolean[] used = new boolean[length];
+        return backtrack(matchsticks, target, 0, used, 0,4);
+
     }
 
-    private boolean backtrack(int[] matchsticks, int index, int[] sides, int target) {
-        if (index == matchsticks.length) {
-            // All matchsticks used, check if all sides equal target
-            return sides[0] == target && sides[1] == target &&
-                   sides[2] == target && sides[3] == target;
+    private boolean backtrack(int[] matchsticks, int target, int start, boolean[] used, int total, int buckets){
+        if(buckets==0)return true;
+        if(total==target){
+            return backtrack(matchsticks,target, 0, used,0,buckets-1);
         }
+        for(int i = start; i<matchsticks.length; i++){
+            if(used[i])continue;
+            if(matchsticks[i]+total>target) break;
+            if(i>0 && matchsticks[i]==matchsticks[i-1]&& !used[i-1]) continue;
+            used[i]=true;
+            if(backtrack(matchsticks, target, i+1, used, total+matchsticks[i], buckets)) return true;
+            used[i] = false;
+            if (total == 0) break;
 
-        for (int i = 0; i < 4; i++) {
-            if (sides[i] + matchsticks[index] <= target) {
-                sides[i] += matchsticks[index];
-                if (backtrack(matchsticks, index + 1, sides, target)) return true;
-                sides[i] -= matchsticks[index]; // backtrack
-            }
+
         }
-
         return false;
+
+
     }
 }
