2cebf9e222ac5882489529a7179625fd7d26802c
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-10 10:40:56 +0530

    Time: 40 ms (58.31%), Space: 40.3 MB (73.6%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.java
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.java
@@ -1,28 +1,18 @@
 class Solution {
     public int countArrangement(int n) {
-        int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = i + 1;
-        }
-        int used[] = new int[n];
-        int a[] = {0};
-        backtrack(new ArrayList<>(), arr, n, used, a);
-        return a[0];
+        boolean used[]=new boolean[n+1];
+        return backtrack(1,n,used);
     }
-
-    public static void backtrack(List<Integer> lst, int num[], int n, int used[], int a[]) {
-        if(lst.size()==n){a[0]+=1; return ;}
-        for(int i=0;i<num.length;i++){
-            if(used[i]==0){
-                lst.add(num[i]);
-                if(num[i]%lst.size() ==0 || lst.size() % num[i] ==0){
-                    used[i]=1;
-                    backtrack(lst,num,n,used,a);
-                    used[i]=0;
-                }
-                lst.remove(lst.size()-1);
+    public static int backtrack(int pos,int n,boolean used[]){
+        if(pos>n){return 1;}
+        int cnt=0;
+        for(int i=1;i<=n;i++){
+            if(!used[i] && (i%pos==0 || pos%i==0)){
+                used[i]=true;
+               cnt+= backtrack(pos+1,n,used);
+                used[i]=false;
             }
         }
+        return cnt;
     }
 }
-
