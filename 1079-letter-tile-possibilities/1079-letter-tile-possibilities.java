745f3c05691f30eda03cb30a7b83bf8d182c295e
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-10 23:23:49 +0530

    Time: 13 ms (39.49%), Space: 41.6 MB (68.28%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -6,18 +6,18 @@ class Solution {
         int a[]={0};
         boolean used[]=new boolean[str.length()];
         for(int i=1;i<=str.length();i++){
-            dfs(str,used,i,a,"");
+            dfs(str,used,i,a,0);
             System.out.println(a[0]);
         }
         return a[0];
     }
-    public static void dfs(String str,boolean used[],int n,int a[],String s){
-        if(s.length()==n){a[0]++;}
+    public static void dfs(String str,boolean used[],int n,int a[],int cnt){
+        if(cnt==n){a[0]++;}
         for(int i=0;i<str.length();i++){
             if(used[i]) continue;
             if(i>0 && str.charAt(i-1)==str.charAt(i) && !used[i-1])continue;
             used[i]=true;
-            dfs(str,used,n,a,s+str.charAt(i));
+            dfs(str,used,n,a,cnt+1);
             used[i]=false;
         }
     }
