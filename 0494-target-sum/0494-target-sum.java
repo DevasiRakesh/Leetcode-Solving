d5c8572b9bb52e5816b420cdcea10798dded0597
DevasiRakesh <devasirakhi123@gmail.com>
2025-08-10 11:07:06 +0530

    Time: 566 ms (28.85%), Space: 41.7 MB (61.89%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,21 +1,16 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int a[]={0};
-        backtrack(new ArrayList<>(),nums,target,0,a,0);
+        backtrack(nums,target,0,a,0);
        return a[0];
     }
-    public static void backtrack(List<Integer> lst,int nums[],int tar,int total,int a[],int idx){
-        if(lst.size()==nums.length && total==tar){
-            a[0]=a[0]+1;
+    public static void backtrack(int nums[],int tar,int total,int a[],int idx){
+        if(idx==nums.length){
+            if( total==tar){a[0]=a[0]+1;}
+            
             return ;
         }
-        for(int i=idx;i<nums.length;i++){
-                lst.add(nums[i]);
-                backtrack(lst,nums,tar,total+nums[i],a,i+1);
-                lst.remove(lst.size()-1);
-                lst.add(-nums[i]);
-                backtrack(lst,nums,tar,total-nums[i],a,i+1);
-                lst.remove(lst.size()-1);
+         backtrack(nums,tar,total+nums[idx],a,idx+1);
+         backtrack(nums,tar,total-nums[idx],a,idx+1);
         }
-    }
-}
+ }
