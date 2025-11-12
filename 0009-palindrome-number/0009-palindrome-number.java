6980cd85fc12b5a1dfd9c2cd2a9ca3572b785690
DevasiRakesh <devasirakhi123@gmail.com>
2025-11-12 21:46:45 +0530

    Time: 5 ms (66.24%), Space: 45.8 MB (11.02%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0){ return false;}
+         int rev_no=0,org=x;
+         while(x!=0){
+            rev_no=rev_no*10+x%10;
+            x/=10;
+         }
+         return (org == rev_no)? true : false;
+    }
+}
